6a35820f66fffb822ceed72a0f49c141a60f1385
sergei-doroshenko <6823298@gmail.com>
2019-10-20 11:06:29 +0300

    Read ini config

--- filerename/data/settings.py
+++ filerename/data/settings.py
@@ -1,8 +1,17 @@
 # Simple class
+import configparser
 from datetime import date
 
 
 class Settings:
+    config = {}
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.config = configparser.ConfigParser()
+        self.config.read('./resources/config.ini')
+
     def now(self):
+        print(self.config['DATABASE']['HOST'])
         today = date.today()
         return today
