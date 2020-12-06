699d7e19fd159c9e0bf43bd20cbabd36a70d39f3
sergei-doroshenko <6823298@gmail.com>
2020-12-06 13:45:44 +0100

    Disk usage utility

--- filerename/scripts/du.py
+++ filerename/scripts/du.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python3
 import os
 import sys
+import time
 
 
 def get_directory_size(directory):
@@ -10,12 +11,13 @@ def get_directory_size(directory):
     try:
         # print("[+] Getting the size of", directory)
         for entry in os.scandir(directory):
-            if entry.is_file():
-                # if it's a file, use stat() function
-                total += entry.stat().st_size
-            elif entry.is_dir():
-                # if it's a directory, recursively call this function
-                total += get_directory_size(entry.path)
+            if not os.path.islink(entry):
+                if entry.is_file():
+                    # if it's a file, use stat() function
+                    total += entry.stat().st_size
+                elif entry.is_dir():
+                    # if it's a directory, recursively call this function
+                    total += get_directory_size(entry.path)
     except NotADirectoryError:
         # if `directory` isn't a directory, get the file size then
         return os.path.getsize(directory)
@@ -39,20 +41,26 @@ def get_size_format(b, factor=1024, suffix="B"):
     return f"{b:05.2f}Y{suffix}"
 
 
-def du(path):
+def du(scan_path):
     """Disk usage in human readable format (e.g. '2,1GB')"""
-    print(f"Scanning {path} ...")
+    print(f"Scanning {scan_path} ...")
+    start = time.time()
+    total_size = 0
     result = []
-    for entry in os.scandir(path):
+
+    for entry in os.scandir(scan_path):
         path = entry.path
-        # size = get_size_format(get_directory_size(path))
         size = get_directory_size(path)
+        total_size += size
         result.append((path, size))
 
     result.sort(key=lambda _: _[1], reverse=True)
 
     for tup in result:
         print('{0:100} - {1}'.format(tup[0], get_size_format(tup[1])))
+
+    end = time.time()
+    print(f"Total {scan_path} size={get_size_format(total_size)}, Scan time={end - start:.2f}sec")
 
 
 if __name__ == "__main__":
